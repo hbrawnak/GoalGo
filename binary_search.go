4aabea589b0f6fa7b7ae233051232bb949b41cdd
Habib <habib@tb-bd.com>
2019-12-05 15:40:28 +0600

    Binary on going

--- binary_search.go
+++ binary_search.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type ElementNotFoundERROR struct {
 	er string
@@ -10,11 +13,31 @@ func (e *ElementNotFoundERROR) Error() string  {
 	return e.er
 }
 
-func BinarySearch()  {
-	
+type resp struct {
+	val int
+}
+
+func BinarySearch(numbers []int,  find int) (*resp, error)  {
+
+	for _, value := range numbers {
+		low := 0
+		high := len(numbers) - 1
+		mid := low + (high - low) / 2
+
+		if numbers[mid] == find {
+			return &resp{val: numbers[mid]}, nil
+		}
+
+	}
+	return &resp{val: mid}, nil
 }
 
 
 func main() {
-	fmt.Println("Binary")
+	numbers := []int{0, 2, 3, 5, 6, 7, 9, 10, 11, 27, 15, 30, 17, 18, 20, 23, 25}
+	sort.Ints(numbers)
+
+	find := 18
+
+	fmt.Println(BinarySearch(numbers, find))
 }
